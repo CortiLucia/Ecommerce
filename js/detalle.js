073f27e61ef3e47277ca8e21304582b972c43b31
Lucia Corti <lulycorti67@gmail.com>
2023-09-30 23:56:27 -0300

    arreglos puntuacion y fondo imagen

--- js/detalle.js
+++ js/detalle.js
@@ -9,11 +9,19 @@ window.addEventListener("load", function () {
     div.classList.add("producto");
     div.innerHTML = `
         <h3 class="nombre">${producto.nombre}</h3>
-        <p class="puntuacion">Condición ${producto.puntuacion}</p>
+        <p class="puntuacion">Condición ${cargarEstrellas().innerHTML}</p>
         <img class="imagen" src="${producto.imgs}" alt="${producto.nombre}">
         <p class="descripcion">${producto.descripcion}</p>
         <a class="volver" href="./index.html"><i class="fa-solid fa-circle-arrow-left"></i>Volver</a>
     `;
     mostrarProducto.append(div);
+  }
+
+  function cargarEstrellas() {
+    const div = document.createElement("div");
+    for (let index = 0; index < producto.puntuacion; index++) {
+      div.innerHTML += '⭐'
+    }
+    return div
   }
 });
